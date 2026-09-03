c71ecf72f66231c645c7b480ae14e81c89bed760
Andrey Sarantsev <50476640+asarantsev@users.noreply.github.com>
2026-09-02 23:38:55 -0700

    Update YJX-vol.py

--- BCX/YJX-vol.py
+++ BCX/YJX-vol.py
@@ -15,36 +15,19 @@ vol = DF['Volatility'].values[1:]
 RegVol = OLS(np.diff(np.log(vol)), pd.DataFrame({'const' : 1, 'lag' : np.log(vol)[:-1]})).fit()
 print(RegVol.summary())
 
-# and apply Yeo-Johnson transform to exponentiated residuals
+# and apply Yeo-Johnson transform to residuals
 volresid = RegVol.resid
 nresid = scipy.stats.yeojohnson(volresid)[0]
 la = scipy.stats.yeojohnson(volresid)[1]
 print('lambda = ', la)
+
+# apply the inverse transform to test this function
+fresid = YJinv(nresid, la)
+plt.plot(fresid, volresid, 'o')
+plt.show()
 
 # test for Gaussianity
 qqplot(nresid, line = 's')
 plt.show()
 print('Shapiro-Wilk test p = ', scipy.stats.shapiro(nresid)[1])
 print('Jarque-Bera test p = ', scipy.stats.jarque_bera(nresid)[1])
-
-# now apply the Yeo-Johnson transform to the volatility data
-nvol = scipy.stats.yeojohnson(vol)[0]
-la = scipy.stats.yeojohnson(vol)[1]
-print('lambda = ', la)
-
-# fit autoregression of order 1 to logarithms of transformed volatility
-RegNVol = OLS(np.diff(np.log(nvol)), pd.DataFrame({'const' : 1, 'lag' : np.log(nvol)[:-1]})).fit()
-print(RegNVol.summary())
-nvolresid = RegNVol.resid
-
-# analyze residuals for Gaussianity
-qqplot(nvolresid, line = 's')
-plt.show()
-print('Shapiro-Wilk test p = ', scipy.stats.shapiro(nvolresid)[1])
-print('Jarque-Bera test p = ', scipy.stats.jarque_bera(nvolresid)[1])
-
-# and for IID
-plot_acf(nvolresid)
-plt.show()
-plot_acf(abs(nvolresid))
-plt.show()
